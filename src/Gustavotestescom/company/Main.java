308a4e27375549bd26172d96e4bf67bc4a5e333e
Gustavo0707 <gustavomelo0703@gmail.com>
2021-05-06 18:28:43 -0300

    Initial commit

---
+++ src/Gustavotestescom/company/Main.java
@@ -0,0 +1,16 @@
+package Gustavotestescom.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int f;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type your temperature in fahrenheit");
+        f = scanner.nextInt();
+        9
+        System.out.println("The temperature in celsius is " + 5 * ((f-32) / 9));
+    }
+}
